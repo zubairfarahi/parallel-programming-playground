ad2cc7c29c0f1a899adf2b9c77ad6e2fb6da22ce
Halil İbrahim Şener <hisener@yahoo.com>
2016-10-23 15:34:09 +0300

    comments are added and some code refactor

--- pthread/0_helloworld/main.c
+++ pthread/0_helloworld/main.c
@@ -1,22 +1,33 @@
+/**
+ * Parallel Hello World with pthread library.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 
-// TODO: write comments
-
 void* hello_work (void* argument);
 
+/**
+ * Main function
+ * @param argc argument count
+ * @param argv argumnet vector
+ * @return status
+ */
 int main (int argc, char** argv) {
 
     if (argc != 2) {
         printf("Usage: %s <thread_number>\n", argv[0]);
-        exit(1);
+        return 1;
     }
 
-    int thread_count = atoi(argv[1]) - 1; // we have master thread also
-    pthread_t* threads = malloc(thread_count * sizeof(pthread_t));
-    int thread_args[thread_count];
+    int thread_count = atoi(argv[1]) - 1; // we have also master thread
 
+    // Allocate memory for threads and threads' arguments
+    pthread_t* threads = malloc(thread_count * sizeof(pthread_t));
+    int* thread_args = malloc(thread_count * sizeof(int));
+
+    // Create threads
     for (int i = 0; i < thread_count; i++) {
         thread_args[i] = i;
         pthread_create(threads + i, NULL, hello_work, thread_args + i);
@@ -24,20 +35,30 @@ int main (int argc, char** argv) {
 
     hello_work(NULL);
 
+    // Wait for each thread to finish the work
     for (int i = 0; i < thread_count; i++) {
         pthread_join(threads[i], NULL);
     }
 
-    return 0;
+    // Deallocate the allocated memory
+    free(threads);
+    free(thread_args);
+
+    pthread_exit(NULL);
 }
 
-void* hello_work (void* argument) {
-    if (argument != NULL) {
-        printf("Hello World from Thread <%d>\n", *((int*) argument));
-        return NULL;
+/**
+ * Hello work (routine) function
+ * @param arg argument for the thread
+ * @return NULL
+ */
+void* hello_work (void* arg) {
+    if (arg != NULL) {
+        int index = *((int*) arg);
+        printf("Hello World from Thread <%d>\n", index);
+    } else {
+        printf("Hello World from Master Thread\n");
     }
 
-    printf("Hello World from Master Thread\n");
-
-    return NULL;
+    pthread_exit(NULL);
 }
